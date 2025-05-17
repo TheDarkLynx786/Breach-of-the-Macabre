667685e3890c98a5ef9b79029e6e4c1446c74ed4
Murtaza Haque <MurtazaHaque@my.unt.edu>
2025-05-17 18:25:54 -0500

    Added Undead Movement
    - Added a little cretin that follows

---
+++ breach-of-the-macabre/Scripts/undead.gd
@@ -0,0 +1,32 @@
+extends CharacterBody2D
+
+
+const SPEED = 150.0
+
+
+var player: CharacterBody2D
+var sprite: AnimatedSprite2D
+
+func _ready() -> void:
+	player = get_node("/root/Game/Warlock")
+	sprite = get_child(0)
+
+func _physics_process(delta: float) -> void:
+	var direction := (player.global_position - global_position).normalized()
+	velocity = direction * SPEED
+	
+	# Look at player
+	var x_dir = direction.x
+	if(x_dir < 0):
+		sprite.flip_h = true
+	if(x_dir > 0):
+		sprite.flip_h = false
+	
+	# Animations
+	if(velocity.length() == 0):
+		sprite.play("Scout-Idle")
+	else:
+		sprite.play("Scout-Move")
+	
+
+	move_and_slide()
